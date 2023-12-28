892d8105a091c42861c5940661c54604286d92d5
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2023-12-28 11:36:40 +0900

    :sparkles: Feat: create model taring step (#5)

--- steps/model_train.py
+++ steps/model_train.py
@@ -3,11 +3,35 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
+
 @step
-def train_model(df: pd.DataFrame) -> None:
+def train_model(
+        X_train: pd.DataFrame,
+        X_test: pd.DataFrame,
+        y_train: pd.DataFrame,
+        y_test: pd.DataFrame,
+        config: ModelNameConfig,
+) -> RegressorMixin:
     """
     Trains the model on the ingested data
 
-    :param df: the ingested data
+    Args:
+        X_train: Training data
+        X_test: Test data
+        y_train: Training labels
+        y_test: Test labels
     """
-    pass
+    try:
+        model = None
+        if config.model_name == 'LinearRegression':
+            model = LinearRegressionModel()
+            train_model = model.train(X_train, y_train)
+            return train_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("Error in training model: {}".format(e))
+        raise e
